1459864bc943a1f4848d50b0c2706b580d06314e
Ketivee-org <metaketivee@gmail.com>
2024-11-05 04:18:17 +0530

    repo update
    we separate Parth ai folder in github so if any want work with parth only he/she can work

--- ai_models/parth/ai_models/parth/routes.py
+++
@@ -1,7 +0,0 @@
-@app.route('/')
-def home():
-    return "Welcome to ParthAI Remote Control API!"
-
-@app.route('/favicon.ico')
-def favicon():
-    return '', 204  # Return an empty response for favicon
